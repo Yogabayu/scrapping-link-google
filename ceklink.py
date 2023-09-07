84c0eb5f59898b0b9358f7daee1eb067f480be3b
Yogabayu <yogabayusbi@gmail.com>
2023-09-07 11:12:17 +0700

    first commit

---
+++ ceklink.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_google_search_links(query):
+    google_search_url = f"https://www.google.com/search?q={query}"
+
+    response = requests.get(google_search_url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = soup.find_all('a')
+        all_links = []
+        for link in links:
+            href = link.get('href')
+            if href and href.startswith('/url?q='):
+                url = href[7:].split('&')[0]
+                all_links.append(url)
+
+        return all_links
+
+    else:
+        print("Gagal mengambil halaman web")
+        return []
+
+print("===================================")
+print("========== Web Scraping ===========")
+print("===================================")
+print("\n")
+print("Contoh untuk mencari situs yang mengandung kata 'slot' => site:dpr.go.id slot")
+print("\n")
+
+query = input("Masukkan query pencarian Google: \n")
+
+links = get_google_search_links(query)
+
+if len(links) > 0:
+    print("\nHasil Pencarian:")
+    for i, link in enumerate(links, start=1):
+        print(f"{i}. {link}")
+else:
+    print("\nTidak ditemukan hasil pencarian.")
+
